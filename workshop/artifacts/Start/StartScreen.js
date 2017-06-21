1166a021e6b76d67c4a10ccf388dff5b3adfa5ce
Eliasz Sawicki <eliasz.sawicki@gmail.com>
2017-06-21 08:41:24 +0200

    Use app state in StartScreen component

--- workshop/artifacts/Start/StartScreen.js
+++ workshop/artifacts/Start/StartScreen.js
@@ -1,14 +1,27 @@
 import * as React from 'react';
 import { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-export default class App extends Component {
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { connect } from 'react-redux';
+class App extends Component {
+    updateText() {
+        // UpdateTextAction('text')
+        // console.log('Update')
+    }
     render() {
         return (React.createElement(View, { style: styles.container },
             React.createElement(Text, null, "Open up App.jss to start working on your app!"),
             React.createElement(Text, null, "Changes you make will automatically reload."),
-            React.createElement(Text, null, "Shake your phone to open the developer menu.")));
+            React.createElement(Text, null, "Shake your phone to open the developer menu."),
+            React.createElement(TouchableHighlight, null,
+                React.createElement(Text, { onPress: this.updateText }, this.props.text))));
     }
 }
+function mapStateToProps(state) {
+    return {
+        text: state.startState.text
+    };
+}
+export default connect(mapStateToProps, undefined)(App);
 const styles = StyleSheet.create({
     container: {
         flex: 1,
--- workshop/src/Start/StartScreen.tsx
+++ workshop/src/Start/StartScreen.tsx
@@ -1,24 +1,51 @@
 import * as React from 'react';
-import {Component} from 'react'
-import { StyleSheet, Text, View, ViewStyle } from 'react-native';
+import { Component } from 'react'
+import { StyleSheet, Text, TouchableHighlight, View, ViewStyle } from 'react-native';
+import { UpdateTextAction } from './startActions'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { AppState } from '../AppState'
 
-export default class App extends Component<void, void> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.jss to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+interface Props {
+    text: string
 }
 
+class App extends Component<Props, void> {
+
+    updateText() {
+        // UpdateTextAction('text')
+        // console.log('Update')
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Open up App.jss to start working on your app!</Text>
+                <Text>Changes you make will automatically reload.</Text>
+                <Text>Shake your phone to open the developer menu.</Text>
+                <TouchableHighlight>
+                    <Text onPress={this.updateText}>
+                        {this.props.text}
+                    </Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
+
+function mapStateToProps(state: AppState) {
+    return {
+        text: state.startState.text
+    }
+}
+
+export default connect(mapStateToProps, undefined)(App)
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  } as ViewStyle
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    } as ViewStyle
 });
