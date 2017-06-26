0ff9de74f7b049f1b7410ff15ff4c5b134479ac7
Andrzej Zabost <andrzej.zabost@brightinventions.pl>
2017-06-26 16:55:05 +0200

    fix example storybook for start screen

--- workshop/src/stories/stories.tsx
+++ workshop/src/stories/stories.tsx
@@ -12,31 +12,19 @@ import List from '../List/List'
 
 declare var module
 
-// storiesOf('Start screen', module)
-//   .add('with short text', () =>
-//     <Start text='test' updateText={() => { }}>
-//     </Start>
-//   )
-//   .add('with !', () =>
-//     <Start text='test!!!' updateText={() => { }}>
-//     </Start>
-//   )
-//   .add('with long text', () =>
-//     <Start text='testing testing testing'
-//       updateText={() => { }}>
-//     </Start>
-//   );
-
-// storiesOf('List', module)
-//   .add('Empty', () =>
-//     <List>
-//     </List>
-//   )
-//   .add('With one element', () =>
-//     <List items={['item']}>
-//     </List>
-//   )
-//   .add('With multiple element', () =>
-//     <List items={['item', 'item2', 'item', 'item2 item2 item2 item2', 'item', 'item2', 'item', 'item2',]}>
-//     </List>
-//   )
+storiesOf('Start screen', module)
+  .add('with short text', () =>
+    <Start
+        updateText={() => {}}
+        downloadData={() => {}}
+        text='test'
+        issues={[{'title': 'a', 'avatarUrl': ''}, {'title': 'b', 'avatarUrl': ''}]}>
+    </Start>
+  )
+  .add('empty listy', () =>
+    <Start
+        updateText={() => {}}
+        downloadData={() => {}}
+        text='test'>
+    </Start>
+  );
